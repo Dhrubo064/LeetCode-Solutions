798d37934e71e6badd18850b068377d446dc52bc
Diganta Das Droba <159650572+Dhrubo064@users.noreply.github.com>
2026-03-09 13:28:50 +0600

    Time: 3 ms (16.14%), Space: 8.5 MB (6.45%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,18 @@
+#include <unordered_set>
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int>seen;
+        while(n!=1 && seen.count(n)==0){
+            seen.insert(n);
+            int sum=0;
+            while(n>0){
+                int digit=n%10;
+                sum=sum+(digit*digit);
+                n=n/10;
+            }
+            n=sum;
+        }
+        return n==1;
+    }
+};
