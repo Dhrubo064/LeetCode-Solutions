6102d24a95f0c431bca04895722f2e4988267da7
Diganta Das Droba <159650572+Dhrubo064@users.noreply.github.com>
2026-03-13 00:22:45 +0600

    Time: 0 ms (100%), Space: 7.9 MB (11.56%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+       if(n==0) return false;
+       while(n!=1){
+        if(n%4!=0){
+            return false;
+        }
+        else{
+            n=n/4;
+        }
+       } 
+       return true;
+    }
+};
